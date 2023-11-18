a84d5d7134ad6a7711f425d07d3d99d5f2d954b2
Krishnava Ghosh <krishnavaghosh23@gmail.com>
2023-11-18 18:56:26 +0530

    Please make this shit work

--- safar_saathi/src/App.jsx
+++ safar_saathi/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
-
+import PlanPage from './components/PlanPage';
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <LandingPage />
+     <div>
+       <PlanPage/>
     </div>
+    
   );
 };
 
