0dccd9bcd6075e4901edead87756f219e6b9c6e0
Eshan276 <eshandas2002@gmail.com>
2023-11-18 22:15:02 +0530

    login connected to database

--- safar_saathi/src/App.jsx
+++ safar_saathi/src/App.jsx
@@ -29,7 +29,30 @@ const App = () => {
         .then(() => {
             setUser(localStorage.getItem("name"));
             setLoggedIn(true);
-        })
+            var myHeaders = new Headers();
+            var user_mail=localStorage.getItem("email")
+      // add content type header to object
+       myHeaders.append("Content-Type", "application/json");
+       // using built in JSON utility package turn object to string and store in a variable
+       var raw = JSON.stringify({"email": user_mail});
+      // create a JSON object with parameters for API call and store in a variable
+       var requestOptions = {
+         method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+       };
+       // make API call with parameters and use promises to get response
+       fetch("https://4qcyee34jd.execute-api.ap-south-1.amazonaws.com/dev/ss_username", requestOptions)
+         .then(response => response.text())
+         .then(result => {
+           const responseBody = JSON.parse(result).body;
+           console.log(responseBody);
+           
+         })
+         .catch(error => console.log('error', error));
+         //console.log(localStorage.getItem("email"))
+        })//hiiiiiiii
         .catch(error => {
             // Handle errors from signInWithGoogle
             console.error("Error signing in with Google:", error);
